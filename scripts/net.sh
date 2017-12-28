647c97a73ff5e8578b33a996280ac9526e2b1b7a
Tabitha Smallwood <tabitha.smallwood@dish.com>
2017-12-28 15:51:47 -0700

    todays eddits

--- scripts/net.sh
+++ scripts/net.sh
@@ -4,10 +4,6 @@ echo "Good Morning Beautiful Meow"
 sudo route add -net 172.16.0.0/16 -interface en4
 sudo route add -net 10.84.0.0/16 -interface en4
 
-#sudo networksetup -setdnsservers Ethernet 10.84.54.200 10.84.54.201 172.16.4.10
-#sudo networksetup -setsearchdomains Ethernet echodata.tv dishacess.tv sling.com uplinkdata.com
-
-
 ssh-add -K
 
 pass git pull origin master
